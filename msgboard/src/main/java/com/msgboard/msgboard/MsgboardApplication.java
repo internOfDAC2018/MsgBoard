d873931aff8e477b2173b37cb18bce3ac87aa153
lyuyu <1969820343@qq.com>
2019-02-21 08:33:06 +0800

    init spring boot server

---
+++ msgboard/src/main/java/com/msgboard/msgboard/MsgboardApplication.java
@@ -0,0 +1,13 @@
+package com.msgboard.msgboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgboardApplication.class, args);
+	}
+
+}
